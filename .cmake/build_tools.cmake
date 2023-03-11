f2a08cc03b8132815f66a2464ab4f006112e7996
Robert Fry <inbox@robertfry.xyz>
2023-03-11 17:14:33 +0000

    Default to Release build

--- .cmake/build_tools.cmake
+++ .cmake/build_tools.cmake
@@ -2,6 +2,11 @@
 # This file contains a list of tools that can be activated and downloaded
 # on-demand. Each tool is enabled during the configuration by passing an
 # additional `-DUSE_<TOOL>=<VALUE>` argument to CMake.
+#
+# Optionas are;
+#  * USE_CLANG_TIDY   Run clang-tidy on source files after each build
+#  * USE_SANITIZERS   Specify a list of sanitizers to compile against
+#
 
 include(${CMAKE_CURRENT_LIST_DIR}/get_cpm.cmake)
 
@@ -25,16 +30,16 @@ endif()
 set(USE_CLANG_TIDY ON CACHE BOOL "Use the clang-tidy static analyzer")
 
 find_program(CLANG_TIDY NAMES "clang-tidy")
-if (CLANG_TIDY)
+if(CLANG_TIDY)
   message(STATUS "Found clang-tidy: ${CLANG_TIDY}")
-  if (NOT USE_CLANG_TIDY)
+  if(NOT USE_CLANG_TIDY)
     message("clang-tidy NOT ENABLED via 'USE_CLANG_TIDY' variable!")
   endif()
 elseif(USE_CLANG_TIDY)
   message(SEND_ERROR "Cannot enable clang-tidy, executable not found!")
 endif()
 
-if (USE_CLANG_TIDY AND CLANG_TIDY)
+if(USE_CLANG_TIDY AND CLANG_TIDY)
   foreach(LANG "C" "CXX" "OBJC" "OBJCXX")
     set(CMAKE_${LANG}_CLANG_TIDY ${CLANG_TIDY})
   endforeach()
