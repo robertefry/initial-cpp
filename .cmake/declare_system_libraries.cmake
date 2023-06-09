e30446e829cf43045630dac7e230d571c406db37
Robert Fry <inbox@robertfry.xyz>
2023-06-09 16:30:51 +0100

    fix: declare_system_library not parsing multiple libraries

--- .cmake/declare_system_libraries.cmake
+++ .cmake/declare_system_libraries.cmake
@@ -1,11 +1,16 @@
 
-function(declare_system_libraries targets)
-  foreach(target ${targets})
-    get_target_property(target_aliased_name ${target} ALIASED_TARGET)
-    if (target_aliased_name)
-      set(target ${target_aliased_name})
-    endif()
-    set_target_properties(${target} PROPERTIES
-      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES $<TARGET_PROPERTY:${target},INTERFACE_INCLUDE_DIRECTORIES>)
+function(declare_system_library target)
+  message(STATUS "Declaring system library ${target}")
+  get_target_property(target_aliased_name ${target} ALIASED_TARGET)
+  if (target_aliased_name)
+    set(target ${target_aliased_name})
+  endif()
+  set_target_properties(${target} PROPERTIES
+    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES $<TARGET_PROPERTY:${target},INTERFACE_INCLUDE_DIRECTORIES>)
+endfunction()
+
+function(declare_system_libraries)
+  foreach(target ${ARGN})
+    declare_system_library(${target})
   endforeach(target)
 endfunction()
